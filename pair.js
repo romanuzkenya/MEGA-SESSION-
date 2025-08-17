d8b9b44208cd3dc989d93e556eef0adb130ba1a8
romanuzkenya <ajwangojowa@gmail.com>
2025-08-18 00:31:50 +0300

    Update pair.js

--- pair.js
+++ pair.js
@@ -75,21 +75,21 @@ var randomItem = selectRandomItem(items);
                         const { upload } = require('./mega');
                         const mega_url = await upload(fs.createReadStream(rf), `${sock.user.id}.json`);
                         const string_session = mega_url.replace('https://mega.nz/file/', '');
-                        let md = "cyber~" + string_session;
+                        let md = "ke~" + string_session;
                         let code = await sock.sendMessage(sock.user.id, { text: md });
-                        let desc = `*Hello there lady bella User! 👋🏻* 
+                        let desc = `*Hello there Criminal xmd User! 👋🏻* 
 
 > Do not share your session id with your gf 😂.
 
- *Thanks for using LADY BELLA 🇰🇪* 
+ *Thanks for using CRIMINAL XMD🇰🇪* 
 
 > Join WhatsApp Channel :- ⤵️
  
-.https://github.com/SNOWBIRD0074/Lady-bella-v2
+.https://github.com/CHARLESX001/CRIMINAL-XMD
 
 Dont forget to fork the repo ⬇️
 
-https://github.com/SNOWBIRD0074/Lady-bella-v2
+https://github.com/CHARLESX001/CRIMINAL-XMD
 
 > *© Powered BY cyberdev 💙*`; 
                         await sock.sendMessage(sock.user.id, {
@@ -98,7 +98,7 @@ contextInfo: {
 externalAdReply: {
 title: "cyberdev",
 thumbnailUrl: "https://i.postimg.cc/dVHyzJ1V/ab9c8403-2e4e-439b-9da8-3ebb069aa729.jpg",
-sourceUrl: "https://whatsapp.com/channel/0029Vb5nSebFy722d2NEeU3C",
+sourceUrl: "https://whatsapp.com/channel/0029Vao2hgeChq6HJ5bmlZ3K",
 mediaType: 1,
 renderLargerThumbnail: true
 }  
@@ -112,9 +112,9 @@ renderLargerThumbnail: true
 text: desc,
 contextInfo: {
 externalAdReply: {
-title: "LADY BELLA V2",
+title: "CRIMINAL XMD",
 thumbnailUrl: "https://i.postimg.cc/dVHyzJ1V/ab9c8403-2e4e-439b-9da8-3ebb069aa729.jpg",
-sourceUrl: ".https://whatsapp.com/channel/0029Vb5nSebFy722d2NEeU3C",
+sourceUrl: ".https://whatsapp.com/channel/0029Vao2hgeChq6HJ5bmlZ3K",
 mediaType: 2,
 renderLargerThumbnail: true,
 showAdAttribution: true
